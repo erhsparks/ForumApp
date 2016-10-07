9fa17c28f9c57ac4e0effa8925ab1cfa80805fb0
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-07 15:21:54 -0700

    Finished Phase 2

--- RedditOnRails/app/controllers/posts_controller.rb
+++ RedditOnRails/app/controllers/posts_controller.rb
@@ -1,37 +1,66 @@
 class PostsController < ApplicationController
   before_action :require_login!
-  
-  def new
 
+  def new
+    @post = Post.new
+
+    render :new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
 
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
 
+    render :edit
   end
 
-  def updated
+  def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      redirect_to post_url(@post)
+    else
+      flash[:errors] = @post.errors.full_messages
+
+      render :edit
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
 
+    render :show
   end
 
   def destroy
+    @post = Post.find(params[:id])
 
-  end
+    if @post
+      @post.destroy
 
-  def index
+      redirect_to sub_url(@post.sub)
+    else
+      flash[:errors] = @post.errors.full_messages
 
+      render :show
+    end
   end
 
   private
 
-  def posts_params
-    params.require(:posts).permit(:title, :url, :content, :sub_id, :author_id)
+  def post_params
+    params.require(:post).permit(:title, :url, :content, :sub_id)
   end
 end
--- RedditOnRails/config/routes.rb
+++ RedditOnRails/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: redirect('http://www.reddit.com')
+
   resources :users
   resource :session, only: [:create, :new, :destroy]
   resources :subs
-  resources :posts
+  resources :posts, only: [:create, :new, :edit, :destroy, :show, :update]
 end
