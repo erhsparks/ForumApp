de617c0187237140ff73da9f30ff13de42e4a667
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-07 16:48:06 -0700

    finish cross-posting

--- RedditOnRails/app/models/post.rb
+++ RedditOnRails/app/models/post.rb
@@ -6,7 +6,6 @@
 #  title      :string           not null
 #  url        :string
 #  content    :text
-#  sub_id     :integer          not null
 #  author_id  :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
